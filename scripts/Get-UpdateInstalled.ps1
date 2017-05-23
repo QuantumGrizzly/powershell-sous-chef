413c566b3825e76e671049c99db5afd87e84442f
QuantumGrizzly <QuantumGrizzly@users.noreply.github.com>
2017-05-23 09:48:52 -0400

    Edit Get-Updates script with WannaCrypt KB's
    Added the Microsoft Knowledge Base (KB) ID to detect if a computer is patched
    against the vulnerability used by WannaCrypt.
    
    - Added all the Microsoft KB's known for WannaCrypt vulnerability
    
    - Change the output value in Verbose

--- scripts/Get-UpdateInstalled.ps1
+++ scripts/Get-UpdateInstalled.ps1
@@ -29,16 +29,42 @@ $MyInvocation | Log-Invocation -Main:$True
 
 #Initialization
 Set-Log -Level 'V2' -Message "Initialize variables"
-$KB = @(4012598,
-  4012212,
-  4012215,
-  4012213,
-  4012216,
-  4012214,
-  4012217,
-  4012606,
-  4013198,
-  4013429)
+
+#KB's relevant to fix WannaCrypt vulnerability
+$KB = @(
+4012598,
+4012212,
+4012215,
+4012213,
+4012216,
+4012214,
+4012217,
+4012606,
+4013198,
+4013389,
+3177186,
+3212646,
+3205401,
+3205409,
+3210720,
+3210721,
+3213986,
+4010096,
+4010096,
+4013429,
+4015217,
+4015438,
+4015549,
+4015550,
+4015551,
+4015552,
+4015553,
+4016635,
+4019215,
+4019216,
+4019264,
+4019472
+)
 Set-Log -Level 'V3' -Message "KB's to search" -Value $KB.Count
 
 #Search for the KB using the class qfe
@@ -54,11 +80,13 @@ Set-Log -Level 'V3' -Message "KB's installed" -Value $InstalledKB.Count
 #Loop for KB
 Set-Log -Level 'V2' -Message "Start KB Loop"
 Foreach ($Id in $KB) {
-  Set-Log -Level 'V3' -Message "Searching KB" -Value $Id
+  #Set-Log -Level 'V3' -Message "Searching KB" -Value $Id
   $Result = $InstalledKB | Where-Object {$_.HotFixID -match "$Id"}
   If ($Result) {
     Set-Log -Level 'O2' -Message "KB Found:" -Value $Id
     Set-Log -Level 'O2' -Message "Installed On:" -Value $Result.InstalledOn
+  } Else {
+    Set-Log -Level 'V3' -Message "KB not Found:" -Value $Id
   }
 
 #>
